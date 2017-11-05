a35e057cc8c1779bf779c894ef5d74ff433d22b9
teppei696 <teppei6969@gmail.com>
2017-11-05 14:22:13 +0900

    レコード取得

--- web/pushkairannbann.php
+++ web/pushkairannbann.php
@@ -5,6 +5,28 @@ $chounaikai = $_REQUEST['chounaikai'];
 error_log("kairannbann: " . $kairannbann);
 error_log("chounaikai: " . $chounaikai);
 
+// 同じ町内会に所属する人を取得
+// kintoneに登録
+define("SUB_DOMAIN", "oas7b.cybozu.com");
+define("APP_NO", "18");
+define("API_TOKEN", "Ok3io8SZAXGWa6CXjd4q1z08Y50FDuZIak7AkhD2");
+// 既存データの検索
+$options = array(
+  'http'=>array(
+    'method'=>'GET',
+    "header"  => "X-Cybozu-API-Token: ". API_TOKEN
+  )
+);
+$context = stream_context_create($options);
+// サーバに接続してデータを貰う。条件式の部分だけURLエンコードを行う(イコールを使っているので）
+$query = urlencode("chounaikai=" . $userid);
+$url = 'https://'. SUB_DOMAIN .'/k/v1/records.json?app='. APP_NO  ."&". $query;
+error_log($url);
+$contents = file_get_contents($url,FALSE,$context);
+$data = json_decode($contents, true);
+error_log($contents);
+//レコードがあったらUPDATE
+
 $accessToken = 'eA/51fwfAvW5dzxv8/TRF1To1ThZEBHQnyEXvMHapZTgSOwG9F2JCFz9mzeMb5H3QRN4YC/VdZX9bx1t8IMPpX/hR5+gl4pKR55CmfWX8+BpT+1WvwN3ny3Dz8oW0cYanOw3F/PJzT1iwaenUSGmcAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v2/bot/message/push';
 // receive json data from line webhook
