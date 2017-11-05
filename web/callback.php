d2284ed1599fcd0f9552932833fd98f79822ad75
teppei696 <teppei6969@gmail.com>
2017-11-05 13:10:06 +0900

    一応存在確認してみよう

--- web/callback.php
+++ web/callback.php
@@ -15,28 +15,44 @@ error_log("chounaikai: " . $chounaikai);
 
 // 送られてきたメッセージの中身からレスポンスのタイプを選択
 if (strpos($message->{"text"},'町内会') !== false) {
-    // 確認ダイアログタイプ
-		$messageData = array('type' => 'text',
-		                 'text' => '町内会の登録ありがとうございます！');
-		// kintoneに登録
-		error_log("insert");
-		// 自分のkintoneの設定
-		define("SUB_DOMAIN", "oas7b.cybozu.com");
-		define("APP_NO", "18");
-		define("API_TOKEN", "Ok3io8SZAXGWa6CXjd4q1z08Y50FDuZIak7AkhD2");
-		//HTTPヘッダ(新規レコードはPOST)
+	// 確認ダイアログタイプ
+	$messageData = array(
+		'type' => 'text',
+		'text' => '町内会の登録ありがとうございます！');
+	// kintoneに登録
+	define("SUB_DOMAIN", "oas7b.cybozu.com");
+	define("APP_NO", "18");
+	define("API_TOKEN", "Ok3io8SZAXGWa6CXjd4q1z08Y50FDuZIak7AkhD2");
+	// 既存データの検索
+	$options = array(
+		'http'=>array(
+			'method'=>'GET',
+			'header'=> "X-Cybozu-API-Token:". API_TOKEN ."\r\n"
+		)
+	);
+	$context = stream_context_create( $options );
+	// サーバに接続してデータを貰う。条件式の部分だけURLエンコードを行う(イコールを使っているので）
+	$query = urlencode("userid=" . $userid);
+	$url = 'https://'. SUB_DOMAIN .'/k/v1/records.json?app='. APP_NO  ."&query=". $query;
+	$contents = file_get_contents($url, FALSE, $context );
+	//JSON形式からArrayに変換
+	$data = json_decode($contents, true);
+
+	//レコードがあったらUPDATE
+	if(!empty($data['records'])){
+		error_log("update");
 		$options = array(
 			'http'=>array(
-				'method'=> 'POST',
+				'method'=> 'PUT',
 				"header"  => "X-Cybozu-API-Token: ". API_TOKEN ."\r\n" . 'Content-Type: application/json',
 				'content' => json_encode(
-						array(
-								'app' => APP_NO,
-								'record' => array(
-															"userid" => array("value" => $userid),    //このフィールドが主キー扱い
-															"chounaikai" => array("value" =>$chounaikai)
-														)
+					array(
+						'app' => APP_NO,
+						'id' => $data['records'][0]['$id']['value'],
+						'record' => array(
+							"chounaikai" => array("value" =>$chounaikai)
 						)
+					)
 				)
 			)
 		);
@@ -44,6 +60,29 @@ if (strpos($message->{"text"},'町内会') !== false) {
 		$url = "https://" . SUB_DOMAIN . "/k/v1/record.json";
 		$contents = file_get_contents($url, FALSE, $context );
 		error_log($contents);
+	}else{
+		error_log("insert");
+		//HTTPヘッダ(新規レコードはPOST)
+		$options = array(
+			'http'=>array(
+				'method'=> 'POST',
+				"header"  => "X-Cybozu-API-Token: ". API_TOKEN ."\r\n" . 'Content-Type: application/json',
+				'content' => json_encode(
+					array(
+						'app' => APP_NO,
+						'record' => array(
+							"userid" => array("value" => $userid),    //このフィールドが主キー扱い
+							"chounaikai" => array("value" =>$chounaikai)
+						)
+					)
+				)
+			)
+		);
+		$context = stream_context_create( $options );
+		$url = "https://" . SUB_DOMAIN . "/k/v1/record.json";
+		$contents = file_get_contents($url, FALSE, $context );
+		error_log($contents);
+	}
 } else {
 	// 確認ダイアログタイプ
 	$messageData = array(
