27ad65ad39e105a4a192a6fd7f04326e07851483
teppei696 <teppei6969@gmail.com>
2017-11-05 14:44:07 +0900

    取得できたユーザだけおくる

--- web/pushkairannbann.php
+++ web/pushkairannbann.php
@@ -25,33 +25,40 @@ error_log($url);
 $contents = file_get_contents($url,FALSE,$context);
 $data = json_decode($contents, true);
 error_log($contents);
-//レコードがあったらUPDATE
 
-$accessToken = 'eA/51fwfAvW5dzxv8/TRF1To1ThZEBHQnyEXvMHapZTgSOwG9F2JCFz9mzeMb5H3QRN4YC/VdZX9bx1t8IMPpX/hR5+gl4pKR55CmfWX8+BpT+1WvwN3ny3Dz8oW0cYanOw3F/PJzT1iwaenUSGmcAdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v2/bot/message/push';
-// receive json data from line webhook
-$raw = file_get_contents('php://input');
-$receive = json_decode($raw, true);
-// parse received events
-$event = $receive['events'][0];
-// build request headers
-$headers = array('Content-Type: application/json',
-                 'Authorization: Bearer ' . $accessToken);
-// build request body
-$message = array('type' => 'text',
-                 'text' => $kairannbann);
-$body = json_encode(array(
-  'to'       => "U1de78326330dc1ad99d3208ead146f73",
-  'messages' => array($message)
-));
-$options = array(CURLOPT_URL            => $url,
-                 CURLOPT_CUSTOMREQUEST  => 'POST',
-                 CURLOPT_RETURNTRANSFER => true,
-                 CURLOPT_HTTPHEADER     => $headers,
-                 CURLOPT_POSTFIELDS     => $body);
-$curl = curl_init();
-curl_setopt_array($curl, $options);
-$result = curl_exec($curl);
-error_log('result: ' . $result);
-curl_close($curl);
+if(!empty($data['records'])){
+  for ($i = 0 ; $i < count($data['records']); $i++) {
+    $userid = $data['records'][$i]['userid']['value']
+    error_log("userid: " . $userid);
+    $accessToken = 'eA/51fwfAvW5dzxv8/TRF1To1ThZEBHQnyEXvMHapZTgSOwG9F2JCFz9mzeMb5H3QRN4YC/VdZX9bx1t8IMPpX/hR5+gl4pKR55CmfWX8+BpT+1WvwN3ny3Dz8oW0cYanOw3F/PJzT1iwaenUSGmcAdB04t89/1O/w1cDnyilFU=';
+    $url = 'https://api.line.me/v2/bot/message/push';
+    // receive json data from line webhook
+    $raw = file_get_contents('php://input');
+    $receive = json_decode($raw, true);
+    // parse received events
+    $event = $receive['events'][0];
+    // build request headers
+    $headers = array('Content-Type: application/json',
+                     'Authorization: Bearer ' . $accessToken);
+    // build request body
+    $message = array('type' => 'text',
+                     'text' => $kairannbann);
+    $body = json_encode(array(
+      'to'       => $userid,
+      'messages' => array($message)
+    ));
+    $options = array(CURLOPT_URL            => $url,
+                     CURLOPT_CUSTOMREQUEST  => 'POST',
+                     CURLOPT_RETURNTRANSFER => true,
+                     CURLOPT_HTTPHEADER     => $headers,
+                     CURLOPT_POSTFIELDS     => $body);
+    $curl = curl_init();
+    curl_setopt_array($curl, $options);
+    $result = curl_exec($curl);
+    error_log('result: ' . $result);
+    curl_close($curl);
+  }
+}
+
+
 ?>
